3e4dcdc0d3a85d05641d318deff9747242ac1dcc
LAXMAN MANOJ <laxmanmanojt@gmail.com>
2022-07-20 01:19:47 +0530

    addNotes Changes.

--- test/src/app/notes/notes.component.ts
+++ test/src/app/notes/notes.component.ts
@@ -7,12 +7,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./notes.component.scss']
 })
 export class NotesComponent implements OnInit {
+  notesList: any;
+  txt_title: string = '';
+  txt_desc: string = '';
+  constructor(private route: Router) { }
 
-  constructor(private route:Router) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.notesList = [
+      { title: 'A', desc: 'a' }
+    ]
   }
-  funNavigatetoLogin(){
+  funDelRec(rec: any) {
+    this.notesList.splice(this.notesList.indexOf(rec), 1);
+    console.log(this.notesList)
+  }
+
+  funSaveNotes() {
+    let rec = { title: this.txt_title, desc: this.txt_desc };
+    this.notesList.push(rec);
+    this.txt_title = '';
+    this.txt_desc = '';
+  }
+  funNavigatetoLogin() {
     this.route.navigateByUrl('/login');
   }
 }
